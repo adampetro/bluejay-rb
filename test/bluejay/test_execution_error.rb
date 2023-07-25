73fcea551ba7f04a2e76a4e64cd9dd5eee5f38b9
Robert Mosolgo <rdmosolgo@gmail.com>
2023-07-25 14:49:43 -0400

    Add ExecutionError#to_h

---
+++ test/bluejay/test_execution_error.rb
@@ -0,0 +1,17 @@
+# typed: ignore
+# frozen_string_literal: true
+
+require "test_helper"
+
+module Bluejay
+  class TestExecutionError < Minitest::Test
+    def test_it_responds_to_to_h
+      err = Bluejay::ExecutionError.new("Something went wrong", ["root", "field", "0", "thing"])
+      expected_h = {
+        "message" => "Something went wrong",
+        "path" => ["root", "field", "0", "thing"],
+      }
+      assert_equal expected_h, err.to_h
+    end
+  end
+end
